dbe0e50f31b413c0bda38e55a6bc493009b13348
Rashen <rashen598@gmail.com>
2023-12-01 16:02:16 +0530

    created quiz stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_flutter/quiz_app.dart';
+
 void main() {
-    
+  runApp(const Quiz());
 }
---
+++ lib/quiz_app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xff607d8b), Color(0xff3f51b5)],
+              stops: [0.25, 0.75],
+              begin: Alignment.bottomRight,
+              end: Alignment.topLeft,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
